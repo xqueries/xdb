1dcc71cb07596ca3fb4e60eff55e7bc7c595626a
Tim Satke <tim.satke@gmail.com>
2020-08-25 08:42:03 +0200

    Review comment

--- internal/parser/scanner/test/gen.go
+++ internal/parser/scanner/test/gen.go
@@ -207,7 +207,7 @@ func generateKeywordColumn(offset int) token.Token {
 	return genTok{
 		offset: offset,
 		typ:    token.KeywordColumn,
-		value:  caseShuffle("Name"),
+		value:  caseShuffle("Column"),
 	}
 }
 func generateKeywordCommit(offset int) token.Token {
