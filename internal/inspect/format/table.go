31f2229ba5fb575cd400cd183dd8b5e0542aac06
SUMUKHA-PK <sumukhapk46@gmail.com>
2020-11-02 18:09:30 +0530

    this commit starts adding table support to the inspector

--- internal/inspect/format/table.go
+++ internal/inspect/format/table.go
@@ -1,1 +1,13 @@
 package format
+
+import (
+	"strings"
+
+	"github.com/xqueries/xdb/internal/engine/table"
+)
+
+func Table(t table.Table) (string, error) {
+	var sb strings.Builder
+
+	return sb.String(), nil
+}
--- internal/inspect/table.go
+++ internal/inspect/table.go
@@ -1,8 +1,32 @@
 package inspect
 
+import (
+	"fmt"
+
+	"github.com/xqueries/xdb/internal/inspect/format"
+)
+
 // ProcessTableCommand responds to the command related to a table.
 // The response is a pre-formatted string which can be printed
 // on a CLI.
 func (i *Inspector) ProcessTableCommand(arg string) (string, error) {
-	return "", nil
+	table, err := i.e.LoadTable(arg)
+	if err != nil {
+		return "", err
+	}
+
+	rowItr, err := table.Rows()
+	if err != nil {
+		return "", err
+	}
+
+	for {
+		row, err := rowItr.Next()
+		if err != nil {
+			return "", err
+		}
+		fmt.Println(row)
+	}
+
+	return format.Table(table)
 }
