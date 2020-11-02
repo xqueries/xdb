7d0cd039af5b59343d838149fb6485ac175d3c35
tsatke <tim.satke@gmail.com>
2020-11-02 14:08:57 +0100

    #75 Add Info() method to the engine
    (engine.Engine).Info() was added, and returns a freshly computed struct (engine.Info), which holds information about current tables in the database.

---
+++ internal/engine/info_test.go
@@ -0,0 +1,23 @@
+package engine
+
+func (suite *EngineSuite) TestInfo() {
+	suite.RunScript(`CREATE TABLE myTable ( col1 TEXT, col2 INTEGER )`)
+	suite.RunScript(`INSERT INTO myTable VALUES ("hello", 1), ("world", 2)`)
+
+	info, err := suite.engine.Info()
+	suite.NoError(err)
+
+	expectedTableInfo := TableInfo{
+		Page:      3,
+		RowAmount: 2,
+		Name:      "myTable",
+	}
+
+	suite.EqualValues([]string{"myTable"}, info.TableNames())
+	suite.Len(info.Tables(), 1)
+	suite.Equal(expectedTableInfo, info.Tables()[0])
+
+	tbl, ok := info.Table("myTable")
+	suite.True(ok)
+	suite.Equal(expectedTableInfo, tbl)
+}
