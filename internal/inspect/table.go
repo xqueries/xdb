af13dfdfc574d4c884dd1a3981fe23dd0b04abab
SUMUKHA-PK <sumukhapk46@gmail.com>
2020-11-02 22:31:50 +0530

    this commit completes #72

--- internal/inspect/table.go
+++ internal/inspect/table.go
@@ -18,6 +18,7 @@ func (i *Inspector) ProcessTablesCommand() (string, error) {
 // ProcessTableCommand responds to the command related to a table.
 // The response is a pre-formatted string which can be printed
 // on a CLI.
+//
 func (i *Inspector) ProcessTableCommand(arg string) (string, error) {
 	tbl, err := i.e.LoadTable(arg)
 	if err != nil {
